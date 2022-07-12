6a5d961cac5c9d4406cb601dc3c803038b4f1ba5
Andrew Mullins <mullinsand15@gmail.com>
2022-07-12 13:03:19 -0600

    feat: Add render methods and tests

--- lib/cell.rb
+++ lib/cell.rb
@@ -16,16 +16,39 @@ class Cell
     end
 
     def fired_upon?
-        @ship.health != @ship.length
+        @ship.health < @ship.length && @ship.health >= 0
     end
 
     def fire_upon
-        unless @shot_at == true
+        if @shot_at == true
+        elsif
+            @shot_at = true
+            if !empty? 
+                @ship.hit
+            end
         end
-        @shot_at = true
-        if !empty? 
-            @ship.hit
+    end
+    
+    def render(see_ships = false)
+        # Comment out about what each scenario means for furth condensing
+        if empty? == true && see_ships == false && @shot_at == false
+            "."
+        elsif empty? == false && see_ships == true && @shot_at == false
+            "S"
+        elsif empty? == true && see_ships == true && @shot_at == false
+            "."
+        elsif empty? == false && see_ships == false && @shot_at == false
+            "."
+        elsif empty? == true && @shot_at == true
+            "M"
+        elsif empty? == false && @shot_at == true && @ship.sunk? == false
+            "H"
+        elsif empty? == false && @shot_at == true && @ship.sunk? == true
+            "X"
+        else
+            "Something went wrong!!"
         end
     end
         
+
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -35,5 +35,42 @@ describe Cell do
         expect(cell.fired_upon?).to be true
     end
 
+    it 'can be rendered to show a miss' do
+        cell = Cell.new('B4')
+        cruiser = Ship.new("Cruiser", 3)
+        
+        expect(cell.render).to eq('.')
+        expect(cell.render(true)).to eq('.')
+        cell.fire_upon
+
+        expect(cell.render).to eq('M')
+        expect(cell.render(true)).to eq('M')
+    end
+
+    it 'can be rendered to show a hit' do
+        cell = Cell.new('B4')
+        cruiser = Ship.new("Cruiser", 3)
+        cell.place_ship(cruiser)
+
+        expect(cell.render).to eq('.')
+        expect(cell.render(true)).to eq('S')
+        cell.fire_upon
+
+        expect(cell.render).to eq('H')
+        expect(cell.render(true)).to eq('H')
+
+        cell.fire_upon
+        cell.fire_upon
+
+        expect(cell.render).to eq('H')
+        expect(cell.render(true)).to eq('H')
+
+        cruiser.hit
+        cruiser.hit
+
+        expect(cell.render).to eq('X')
+        expect(cell.render(true)).to eq('X')
+    end
+    
 
 end
