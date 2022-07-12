e205df194be0903852711ef4819306e197b1aa30
Sid Mann <sjmann2@outlook.com>
2022-07-11 21:38:02 -0500

    test: add test for ship attributes

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -6,4 +6,12 @@ describe Ship do
 
     expect(cruiser).to be_an_instance_of(Ship)
     end
+
+    it 'has attributes' do
+    cruiser = Ship.new("Cruiser", 3)
+
+    expect(cruiser.name).to eq("Cruiser")
+    expect(cruiser.length).to be(3)
+    end
+
 end
