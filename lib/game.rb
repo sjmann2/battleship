0b03557ef66023d56dbb8b80d61449441e944ad1
Andrew Mullins <mullinsand15@gmail.com>
2022-07-14 19:56:07 -0600

    feat: Add take_turn method and tests and end_game? method

--- lib/game.rb
+++ lib/game.rb
@@ -3,20 +3,80 @@ class Game
               :cruiser_computer,
               :submarine_player,
               :submarine_computer,
-              :board
+              :board_player,
+              :board_computer
   def initialize
-    @cruiser_player = Ship.new("Player's Cruiser", 3)
-    @cruiser_computer = Ship.new("Computer's Cruiser", 3)
-    @submarine_player = Ship.new("Player's Submarine", 2)
-    @submarine_computer = Ship.new("Computer's Submarine", 3)
-    @board = Board.new
+    @cruiser_player = Ship.new("cruiser", 3)
+    @cruiser_computer = Ship.new("cruiser", 3)
+    @submarine_player = Ship.new("submarine", 2)
+    @submarine_computer = Ship.new("submarine", 2)
+    @board_player = Board.new
+    @board_computer = Board.new
   end
 
   def place_ships_player(ship_instance, coordinate_array)
-    if board.valid_placement?(ship_instance, coordinate_array)
-    board.place(ship_instance, coordinate_array)
+    if board_player.valid_placement?(ship_instance, coordinate_array)
+    board_player.place(ship_instance, coordinate_array)
     else
       'Invalid coordinates try again'
+    end
+  end
+
+  def place_ships_computer(ship_instance, coordinate_array)
+    if board_computer.valid_placement?(ship_instance, coordinate_array)
+    board_computer.place(ship_instance, coordinate_array)
+    else
+      'Invalid coordinates try again'
+    end
+  end
+
+  def take_turn(player_shot, computer_shot)
+      puts "=============COMPUTER BOARD============="
+      puts board_computer.render
+      puts "==============PLAYER BOARD=============="
+      puts board_player.render(true)
+
+      if board_computer.valid_coordinate?(player_shot) && !board_computer.cells[player_shot].shot_at
+        board_computer.cells[player_shot].fire_upon
+      else
+        "Something went wrong!"
+      end
+
+      board_player.cells[computer_shot].fire_upon
+      #feedback here!
+      puts shot_feedback_player_line(player_shot)
+      puts shot_feedback_computer_line(computer_shot)
+  end
+
+  def shot_feedback_player_line(player_shot)
+    if board_computer.cells[player_shot].render == 'M'
+      "Your shot on #{player_shot} was a miss"
+    elsif board_computer.cells[player_shot].render == 'H'
+      "Your shot on #{player_shot} was a hit"
+    elsif board_computer.cells[player_shot].render == 'X'
+      "Your shot on #{player_shot} sunk my #{board_computer.cells[player_shot].ship.name}"
+    else
+      "Something went wrong!"
+    end
+  end
+
+  def shot_feedback_computer_line(computer_shot)
+    if board_player.cells[computer_shot].render == 'M'
+      "My shot on #{computer_shot} was a miss"
+    elsif board_player.cells[computer_shot].render == 'H'
+      "My shot on #{computer_shot} was a hit"
+    elsif board_player.cells[computer_shot].render == 'X'
+      "My shot on #{computer_shot} sunk your #{board_player.cells[computer_shot].ship.name}"
+    else
+      "Something went wrong!"
+    end
+  end
+  
+  def end_game?
+    if (@cruiser_player.sunk? && @submarine_player.sunk?) || (@submarine_computer.sunk? && @cruiser_computer.sunk?)
+      true
+    else
+      false
     end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -17,7 +17,9 @@ describe Game do
     expect(game.cruiser_computer).to be_instance_of(Ship)
     expect(game.submarine_player).to be_instance_of(Ship)
     expect(game.submarine_computer).to be_instance_of(Ship)
-    expect(game.board).to be_instance_of(Board)
+    expect(game.board_player).to be_instance_of(Board)
+    expect(game.board_computer).to be_instance_of(Board)
+
   end
 
 
@@ -27,14 +29,87 @@ describe Game do
     #Initialization- both sides place ships
     game.place_ships_player(game.cruiser_player, ['A1', 'A2', 'A3'])
     game.place_ships_player(game.submarine_player, ['B1', 'B2'])
-    expect(game.board.cells['A1'].ship).to eq(game.cruiser_player)
-    expect(game.board.cells['A2'].ship).to eq(game.cruiser_player)
-    expect(game.board.cells['A3'].ship).to eq(game.cruiser_player)
+    expect(game.board_player.cells['A1'].ship).to eq(game.cruiser_player)
+    expect(game.board_player.cells['A2'].ship).to eq(game.cruiser_player)
+    expect(game.board_player.cells['A3'].ship).to eq(game.cruiser_player)
     # expect(game.board.cells['B3'].empty?).to eq(true)
     expect(game.place_ships_player(game.submarine_player, ['B3', 'B2'])).to eq('Invalid coordinates try again')
     
+    game.place_ships_computer(game.cruiser_computer, ['A1', 'A2', 'A3'])
+    game.place_ships_computer(game.submarine_computer, ['B1', 'B2'])
+    expect(game.board_computer.cells['A1'].ship).to eq(game.cruiser_computer)
+    expect(game.board_computer.cells['A2'].ship).to eq(game.cruiser_computer)
+    expect(game.board_computer.cells['A3'].ship).to eq(game.cruiser_computer)
+    expect(game.board_computer.cells['B1'].ship).to eq(game.submarine_computer)
+    expect(game.place_ships_computer(game.submarine_computer, ['B3', 'B2'])).to eq('Invalid coordinates try again')
+
   end
 
+  it 'can take a turn' do
+    game = Game.new
+    game.place_ships_player(game.cruiser_player, ['A1', 'A2', 'A3'])
+    game.place_ships_player(game.submarine_player, ['B1', 'B2'])
+    game.place_ships_computer(game.cruiser_computer, ['A1', 'A2', 'A3'])
+    game.place_ships_computer(game.submarine_computer, ['B1', 'B2'])
+
+    #(player's inputted shot, computer shot)
+    expect(game.board_computer.cells['A4'].render).to eq('.')
+    expect(game.board_player.cells['A4'].render).to eq('.')
+    game.take_turn('A4', 'A4')
+    expect(game.board_computer.cells['A4'].render).to eq('M')
+    expect(game.board_player.cells['A4'].render).to eq('M')
+    game.take_turn('A1', 'B1')
+    expect(game.board_computer.cells['A1'].render).to eq('H')
+    expect(game.board_player.cells['B1'].render).to eq('H')
+    game.take_turn('A2', 'B2')
+    expect(game.board_computer.cells['A2'].render).to eq('H')
+    expect(game.board_player.cells['B2'].render).to eq('X')
+    game.take_turn('C2', 'C2')
+
+    #CLI output and feedback isn't tested, render(true)
+    
+
+
+    #Render both boards
+    #player shot => go into computer board, call the corresponding cell, run fire_upon method
+    #computer shot => go into player board, call the corresponding cell, run fire_upon method
+    #Reporting results of the shot (feedback!)
+    #Runs inside a loop until both ships of either player or computer are ship.sunk? = true
+
+  end
+
+  it 'knows when to end game' do
+    game = Game.new
+    game.place_ships_player(game.cruiser_player, ['A1', 'A2', 'A3'])
+    game.place_ships_player(game.submarine_player, ['B1', 'B2'])
+    game.place_ships_computer(game.cruiser_computer, ['A1', 'A2', 'A3'])
+    game.place_ships_computer(game.submarine_computer, ['B1', 'B2'])
+
+
+    game.take_turn('A2', 'A4')
+    game.take_turn('A1', 'B1')
+    game.take_turn('A3', 'B2')
+    expect(game.end_game?).to eq(false)
+    game.take_turn('B1', 'C2')
+    game.take_turn('B2', 'C3')
+    expect(game.end_game?).to eq(true)
+  end
+
+  #loop of the turn that gets repeated until end_game helper method switches to true
+  #end_game_output
+
+  #Feedback on shots
+  xit 'gives feedback on shots' do
+    game = Game.new
+    game.place_ships_player(game.cruiser_player, ['A1', 'A2', 'A3'])
+    game.place_ships_player(game.submarine_player, ['B1', 'B2'])
+    game.place_ships_computer(game.cruiser_computer, ['A1', 'A2', 'A3'])
+    game.place_ships_computer(game.submarine_computer, ['B1', 'B2'])
+    #take_turn test
+    expect(game.take_turn('A4', 'A4')).to eq("")
+    #helper test
+    game.shot_feedback('A4', 'A4')
+  end
 
 
 
@@ -58,13 +133,7 @@ describe Game do
 
     expect()
   end
-    #Feedback on shots
-  xit 'gives feedback on shots' do
-    game = Game.new
-    game.place_ships_player
-    game.place_ships_computer
 
-  end
 end
 
 
