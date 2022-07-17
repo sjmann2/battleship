695723f6c66f300bcfc1bb3bcec27924b5948a9c
Sid Mann <sjmann2@outlook.com>
2022-07-16 20:45:49 -0500

    fix: update runner with player class

--- lib/game_runner.rb
+++ lib/game_runner.rb
@@ -3,6 +3,7 @@ require "./lib/ship"
 require "./lib/cell"
 require "./lib/game"
 require "./lib/cell_generator"
+require "./lib/player"
 
 def run_game
   game = Game.new
@@ -35,60 +36,69 @@ def run_game
   computer_ship_placement_array = random_computer_ship_placement(game.submarine_computer, game, computer_ship_placement_array)
   game.place_ships_computer(game.submarine_computer, computer_ship_placement_array)
 
-  require "pry"; binding.pry
   #Player places ships
-  p "I have laid out my ships on the grid."
-  p "You now need to lay out your two ships."
-  p "The Cruiser is three units long and the Submarine is two units long."
-  puts game.board_player.render
+
+  puts """I have laid out my ships on the grid.
+      You now need to lay out your two ships.
+      The Cruiser is three units long and the Submarine is two units long."""
+
+  puts game.player.board.render
+
   p "Enter the squares for the Cruiser (3 spaces):"
+
   player_cruiser_placement = gets.chomp.delete(",").upcase.split(" ")
-  until game.board_player.valid_placement?(game.cruiser_player, player_cruiser_placement) == true
+
+  until game.player.board.valid_placement?(game.player.cruiser, player_cruiser_placement) == true
+
     p "Invalid coordinates, please try again."
+
     player_cruiser_placement = gets.chomp.delete(",").upcase.split(" ")
   end
-  game.place_ships_player(game.cruiser_player, player_cruiser_placement)
 
-  puts game.board_player.render(true)
+  game.player.place_ships(game.player.cruiser, player_cruiser_placement)
+
+  puts game.player.board.render(true)
+
   p "Enter the squares for the Submarine (2 spaces):"
+
   player_submarine_placement = gets.chomp.delete(",").upcase.split(" ")
-  until game.board_player.valid_placement?(game.submarine_player, player_submarine_placement) == true
+
+  until game.player.board.valid_placement?(game.player.submarine, player_submarine_placement) == true
+    
     p "Invalid coordinates, please try again."
+
     player_submarine_placement = gets.chomp.delete(",").upcase.split(" ")
   end
-  game.place_ships_player(game.submarine_player, player_submarine_placement)
-  puts game.board_player.render(true)
 
-  #print out communication: ship length, render board,
-  #provide input for ship positions(cruiser, submarine)
-  #If input is invalid, method to have player re-enter coordinates
-  #Provide more options for varied inputs on ship placement
-  #The turn
+  game.player.place_ships(game.player.submarine, player_submarine_placement)
+  puts game.player.board.render(true)
+
   until game.end_game? == true
     computer_shot = game.board_computer.cells.keys.sample
-    until !game.board_player.cells[computer_shot].shot_at == true
+    until !game.player.board.cells[computer_shot].shot_at == true
       computer_shot = game.board_computer.cells.keys.sample
     end
+
     p "Enter the coordinate for your shot:"
+
     player_shot = gets.chomp.upcase
+
     until game.board_computer.valid_coordinate?(player_shot) && !game.board_computer.cells[player_shot].shot_at
       game.board_computer.cells[player_shot].fire_upon == true
+
       p "Invalid coordinates, please try again."
+
       player_shot = gets.chomp.upcase
     end
+
     game.take_turn(player_shot, computer_shot)
   end
 
-  if (game.cruiser_player.sunk? && game.submarine_player.sunk?)
-    p "I won, hahahahah"
+  if (game.player.cruiser.sunk? && game.player.submarine.sunk?)
+    p "I won, hahahahaha"
   else
     p "You won, nice job beating a computer..."
   end
-  #until loop: until end game conditions are met
-  #End game
-  #You won or I won
-  #Returned back to main menu
-
 end
 
 player_input = nil
@@ -103,7 +113,3 @@ until player_input == "q"
   p "Enter p to play. Enter q to quit."
   player_input = gets.chomp
 end
-
-# until end_game? == true
-
-# end
