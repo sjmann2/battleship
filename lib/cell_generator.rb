8841fd570c8971c52933673f2c89f46003ce9f8f
Sid Mann <sjmann2@outlook.com>
2022-07-18 10:35:40 -0500

    fix: syntax of height and length swapped to match render method

--- lib/cell_generator.rb
+++ lib/cell_generator.rb
@@ -8,16 +8,16 @@ class CellGenerator
   end
 
   def cells
-    @height = (1..@height).to_a
-    @length = ("A"..(("A".ord + @length - 1).chr)).to_a
+    @length = (1..@length).to_a
+    @height = ("A"..(("A".ord + @height - 1).chr)).to_a
     #We want "A".."D"
-    #change number(length) into letter
-    #if length = 4, turn 4 into letter D
+    #change number(height) into letter
+    #if height = 4, turn 4 into letter D
     #ordinal value of A = 65, so 65 + length 4-1 = 68
     #68 back into a character = D
     #Turn all of it back into an array
-    @height
-      .map { |num| @length.map { |letter| letter + num.to_s } }
+    @length
+      .map { |num| @height.map { |letter| letter + num.to_s } }
       .flatten!
       .map { |coordinate| [coordinate, Cell.new(coordinate)] }
       .to_h
